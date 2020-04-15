69cd36b5bebd8d0c9252f4efa49a814f174abf0e
Ryan Lam <54342282+lamhochit@users.noreply.github.com>
2020-04-15 23:20:09 +0800

    Finalised

---
+++ lab5/cmake-build-debug/CMakeFiles/lab5.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/lamhochit/CLionProjects/comp-2012/lab5/main.cpp" "C:/Users/lamhochit/CLionProjects/comp-2012/lab5/cmake-build-debug/CMakeFiles/lab5.dir/main.cpp.obj"
+  "C:/Users/lamhochit/CLionProjects/comp-2012/lab5/organization.cpp" "C:/Users/lamhochit/CLionProjects/comp-2012/lab5/cmake-build-debug/CMakeFiles/lab5.dir/organization.cpp.obj"
+  "C:/Users/lamhochit/CLionProjects/comp-2012/lab5/regionx.cpp" "C:/Users/lamhochit/CLionProjects/comp-2012/lab5/cmake-build-debug/CMakeFiles/lab5.dir/regionx.cpp.obj"
+  "C:/Users/lamhochit/CLionProjects/comp-2012/lab5/regiony.cpp" "C:/Users/lamhochit/CLionProjects/comp-2012/lab5/cmake-build-debug/CMakeFiles/lab5.dir/regiony.cpp.obj"
+  "C:/Users/lamhochit/CLionProjects/comp-2012/lab5/who.cpp" "C:/Users/lamhochit/CLionProjects/comp-2012/lab5/cmake-build-debug/CMakeFiles/lab5.dir/who.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab5/cmake-build-debug/CMakeFiles/lab5.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab5.dir/main.cpp.obj"
+  "CMakeFiles/lab5.dir/organization.cpp.obj"
+  "CMakeFiles/lab5.dir/regionx.cpp.obj"
+  "CMakeFiles/lab5.dir/regiony.cpp.obj"
+  "CMakeFiles/lab5.dir/who.cpp.obj"
+  "lab5.exe"
+  "lab5.exe.manifest"
+  "lab5.lib"
+  "lab5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
